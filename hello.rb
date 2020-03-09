3b952d2b880e5888c9462ef3907bf6e03792aa36
opinyo <yotam@opinya.co.il>
2020-03-09 10:09:13 +0200

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -4,6 +4,7 @@ puts 'hello!'
 # puts ENV.inspect
 path = ENV['GITHUB_EVENT_PATH']
 data = JSON.parse(File.read(path))
+p data
 diff = `git diff --unified=0 --no-color HEAD~#{data['commits'].size}`
 patches = GitDiffParser.parse(diff)
 p patches
